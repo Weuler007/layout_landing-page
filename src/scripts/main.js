e46ae9ca9836d2b76d1e881d9e8bd4d17d24b151
Weuler de Oliveira Vieira <jhdlfdf@hotmail.com>
2026-07-01 11:38:17 -0300

    adicionando largura para desktop  do nav e footer

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,10 @@
 'use strict';
 
 import '../styles/main.scss';
+
+const menuCloseButton = document.querySelector('.menu__close');
+
+menuCloseButton?.addEventListener('click', event => {
+  event.preventDefault();
+  window.location.hash = 'top';
+});
